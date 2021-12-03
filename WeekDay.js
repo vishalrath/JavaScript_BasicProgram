75f14c7748e150fabe060abb7a312634882b05a5
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-03 07:50:49 +0530

    [vishal] javascript program using switch statement

---
+++ WeekDay.js
@@ -0,0 +1,31 @@
+const prompt = require('prompt-sync')();
+
+ const number = prompt('Enter a number between 1-7: ');
+ 
+ switch(Number(number)) 
+ {
+     case 1:
+         console.log("Sunday");
+         break;
+     case 2:
+         console.log("Monday");
+         break;
+     case 3:
+         console.log("Tuesday");
+         break;
+     case 4:
+         console.log("Wednesday");
+         break;
+     case 5:
+         console.log("Thursday");
+         break;
+     case 6:
+         console.log("Friday");
+         break;
+     case 7:
+         console.log("Saturday");
+         break;
+     default:
+         console.log("Invalid input.");
+         break;
+ }
